8eeaa428d4c56f4c110ccdd123b7203afc31409f
Azael Ibarra <Azael Ibarra@ACER>
2016-10-18 19:01:53 -0500

    Se le agrego el codigo para la conexion del proyecto, asi como las excepciones y formularios en blanco

--- RestOmeSer/DataAccessLayer/Restaurante/CatProductoDAL.cs
+++ RestOmeSer/DataAccessLayer/Restaurante/CatProductoDAL.cs
@@ -2,11 +2,57 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using sistemaRestaurante.baseDatos;
+using sistemaRestaurante.modelo;
 
 namespace DataAccessLayer.Restaurante
 {
     class CatProductoDAL
     {
+        conexion conexion = new conexion();
+        public List<CatProducto> seleccionarCatProducto()
+        {
+            List<CatProducto> listaCtProducto = new List<CatProducto>();
+
+            SqlCommand Comando = new SqlCommand();
+            Comando.Connection = conexion.openDataBase();
+
+            Comando.CommandType = CommandType.StoredProcedure;
+
+            Comando.CommandText = "sp_selecCatProdut";
+            // CatProducto _catP = new CatProducto();
+            // _catP.Categoria = "Seleccione";
+
+            //   listaCtProducto.Add(_catP);
+
+            try
+            {
+                IDataReader lector = Comando.ExecuteReader();
+                while (lector.Read())
+                {
+                    CatProducto _catProduto = new CatProducto();
+                    _catProduto.Id = int.Parse(lector[0].ToString());
+                    _catProduto.Categoria = lector[1].ToString();
+                    listaCtProducto.Add(_catProduto);
+                }
+            }
+            catch (Exception ex)
+            {
+                string m = ex.Message;
+                Comando.Dispose();
+            }
+
+            finally
+            {
+                conexion.closeDatabase();
+            }
+
+            return listaCtProducto;
+        }
+    }
+}
+
     }
 }
--- RestOmeSer/DataAccessLayer/Restaurante/rolDAL.cs
+++ RestOmeSer/DataAccessLayer/Restaurante/rolDAL.cs
@@ -2,11 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using sistemaRestaurante.baseDatos;
+using sistemaRestaurante.modelo;
 
 namespace DataAccessLayer.Restaurante
 {
     class rolDAL
     {
+        conexion conexion = new conexion();
+        public List<roles> seleccionarCatProducto()
+        {
+            List<roles> listaRol = new List<roles>();
+
+            SqlCommand Comando = new SqlCommand();
+            Comando.Connection = conexion.openDataBase();
+
+            Comando.CommandType = CommandType.StoredProcedure;
+
+            Comando.CommandText = "sp_selectRol";
+            roles _roll = new roles();
+            _roll.Roluser = "Seleccione";
+
+            listaRol.Add(_roll);
+
+            try
+            {
+                IDataReader lector = Comando.ExecuteReader();
+                while (lector.Read())
+                {
+                    roles _rol = new roles();
+                    _rol.Id = int.Parse(lector[0].ToString());
+                    _rol.Roluser = lector[1].ToString();
+                    listaRol.Add(_rol);
+                }
+            }
+            catch (Exception ex)
+            {
+                string m = ex.Message;
+                Comando.Dispose();
+            }
+
+            finally
+            {
+                conexion.closeDatabase();
+            }
+
+            return listaRol;
+        }
+    }
+}
     }
 }
