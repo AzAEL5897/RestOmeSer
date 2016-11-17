08030891ca75d941e510ee2538fd6e05741bd12b
wendy <wendy@WENDY-PC>
2016-11-17 09:41:46 -0600

    hola

---
+++ RestOmeSer/EsteticaOmeSerWeb/login.php
@@ -0,0 +1,112 @@
+<?php
+session_start();
+session_destroy();
+?>
+
+<html>
+<head>
+
+<A name="INICIO"></A>
+    <title>Login</title>
+	
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/layout.css" type="text/css" media="screen"> 
+    <script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+    <script src="js/cufon-yui.js" type="text/javascript"></script>
+    <script src="js/cufon-replace.js" type="text/javascript"></script> 
+    <script src="js/Dynalight_400.font.js" type="text/javascript"></script>
+    <script src="js/FF-cash.js" type="text/javascript"></script>
+    <script src="js/tms-0.3.js" type="text/javascript"></script>
+    <script src="js/tms_presets.js" type="text/javascript"></script>
+    <script src="js/jquery.easing.1.3.js" type="text/javascript"></script>
+    <script src="js/jquery.equalheights.js" type="text/javascript"></script>    
+	<!--[if lt IE 8]>
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+        	<img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+        </a>
+    </div>
+	<![endif]-->
+    <!--[if lt IE 9]>
+   		<script type="text/javascript" src="js/html5.js"></script>
+	<![endif]-->
+</head>
+<body id="page1">
+	<!--==============================header=================================-->
+<meta charset="UTF-8">
+<title> Inicio </title>
+<link href ="css/login.css" rel ="stylesheet" type ="text/css">
+</head>
+<body>
+<section id="Formulario">
+<center><h1> Iniciar Sesión </h1></center>
+<br>
+<br>	<div class="row-bot">
+        	<div class="row-bot-bg">
+            	<div class="main">
+        	<h2>Barberia Estetica OME<span>GA. Server El Royal Fork</span></h2>
+                    
+                                <fieldset>  
+<div class ="login">
+<form action ="../controller/login.php" method= "POST" > 
+<center>    
+    <fieldset>       
+        <legend>Login</legend>	
+		<p>
+                    <input title = "se necesita el nombre" type = "text" name = "usuario" placeholder = "usuario" required>
+                </p> 
+                <p>
+                    <input  title = "se necesita la contraseña" type = "password" name = "pass" placeholder = "password" required>
+                </p> 
+                <p>
+                   <input  type = "submit" value = "Entrar"> 
+                   <input  type = "reset" value = "Limpiar"> 
+                </p>               
+                <p>
+                    <a href ="crear.html">
+                        Crear cuenta
+                 </fieldset>   </a>
+                </p>
+
+</center>
+</form>	
+</div>
+</section>
+</div>
+             
+        
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+</section>
+    
+	<!--==============================footer=================================-->
+    <footer>
+        <h2>" Cualquiera puede hacer una Applicacion "</h2>
+    </footer>
+    </nav>
+                </div>
+            </div>
+        </div>
+        <div class="row-bot">
+        	<div class="row-bot-bg">
+            	<div class="main">
+                	
+                            </ul>
+            </form>
+        </div>
+    </body>
+</html>
+
+    </script>
+
+</body>
+</html>
